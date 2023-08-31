bfb830a47442ee71bbc1d845ac5e025761125add
MateoMazziniOngay <mateomazzini610@gmail.com>
2023-08-31 12:52:19 -0300

    Progress on app

--- citas-react/src/components/ListadoPacientes.jsx
+++ citas-react/src/components/ListadoPacientes.jsx
@@ -1,6 +1,35 @@
 const ListadoPacientes = () => {
   return (
-    <h2>Listado Pacientes</h2>
+    <div className="w-1/2 lg:w-3/5">
+      <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+      <p className="text-xl mt-5 text-center mb-10">
+        Administra tus {''}
+        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+      </p>
+
+      <div className="m-5 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+      </div>
+
+    </div>
   )
 }
 
