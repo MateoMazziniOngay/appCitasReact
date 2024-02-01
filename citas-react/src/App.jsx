f137a24d92102c4572fe39f639d2ad0e8fa5eed0
MateoMazziniOngay <mateomazzini610@gmail.com>
2024-01-31 21:22:33 -0300

    Deleted spaces

--- citas-react/src/App.jsx
+++ citas-react/src/App.jsx
@@ -2,11 +2,7 @@ import Formulario from "./components/Formulario"
 import Header from "./components/Header"
 import ListadoPacientes from "./components/ListadoPacientes"
 
-
-
 function App() {
-
-
   return (
     <div className="container mx-auto mt-20">
       <Header />
